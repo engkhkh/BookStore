d644a2c4cc63a02824abbd14cef8746806f0d887
engkhkh/fullerp <khilel208@gmail.com>
2020-11-15 16:46:08 +0300

    repo1

---
+++ BookStore/Models/Repo/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repo
+{
+    public class BookRepository : IBookStoreRepository<Book>
+    {
+        public void add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> list()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookStore/Models/Repo/IBookStoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repo
+{
+    interface IBookStoreRepository<TEntity>
+    {
+        IList<TEntity> list();
+        TEntity find(int id);
+        void add(TEntity entity);
+
+        void update(TEntity entity);
+
+        void delete(int id);
+
+    }
+}
