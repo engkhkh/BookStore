11a482bca8f4528ad18074e40e947ce1ac537c38
engkhkh/fullerp <khilel208@gmail.com>
2020-11-21 13:35:48 +0300

    21-11

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Models.Repo;
+using BookStore.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookStoreRepository<Book> bookRespo;
+
+        public IBookStoreRepository<Author> authorRepos { get; }
+
+        public BookController(IBookStoreRepository<Book> BookRepossitory, IBookStoreRepository<Author> AuthorRepos)
+        {
+            bookRespo = BookRepossitory;
+            authorRepos = AuthorRepos;
+        }
+        // GET: Book
+        public ActionResult Index()
+        {
+            var books = bookRespo.list();
+            return View(books);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            var book = bookRespo.find(id);
+
+            return View(book);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            var model = new BookAuthorViewModels
+            {
+                Authors = authorRepos.list().ToList()
+            };
+            return View(model);
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookAuthorViewModels model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var author = authorRepos.find(model.AuthorId);
+                Book book = new Book
+                {
+                    Id=model.BookId,
+                    Title=model.Title,
+                    Description=model.Description,
+                    Author=author
+
+                };
+                bookRespo.add(book);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var book = bookRespo.find(id);
+            var viewmodel = new BookAuthorViewModels
+            {
+
+            };
+            return View();
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
